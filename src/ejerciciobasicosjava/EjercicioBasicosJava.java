4a564216ba12be7d52891dffd402c36945ba2b9c
xp <xp@CETYSDAM00>
2019-11-28 18:17:49 +0100

    hasta plantear el 5º ejercicio

--- src/ejerciciobasicosjava/EjercicioBasicosJava.java
+++ src/ejerciciobasicosjava/EjercicioBasicosJava.java
@@ -7,6 +7,57 @@ package ejerciciobasicosjava;
  * ejercicio 1A) el da leas ardillas!
  */
 public class EjercicioBasicosJava {
+    
+    
+    public boolean menorPro(int a,int b, int c){
+       if((a<10) ||(b<10) || (c<10)){
+           
+       }
+             
+            
+            
+        
+       return false; 
+    }
+    
+    
+    
+    public boolean suenaMovil(boolean matinal,boolean madre, boolean dormido){
+        if(matinal && madre && !dormido){
+            return true;
+        }
+        if(!matinal && !madre && dormido ){
+            return false;
+            
+        }
+        if ( dormido){
+            return false;
+            
+        }
+        if(matinal && !madre && !dormido){
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean numeroVanidos(int numero){
+        
+       int numero_1=numero%11;
+       
+       if(numero_1==0){
+           return true;
+       }
+       if (numero_1==1){
+           return true;
+           
+       }
+            
+        return false;
+        
+   
+    }
+    
 
     public boolean fiestaArdilla(int numeroBellotas, boolean finSemana) {
         if (finSemana == true) {
@@ -49,10 +100,18 @@ public class EjercicioBasicosJava {
         System.out.println(ejercicios.fiestaArdilla(50, false));
         System.out.println(ejercicios.fiestaArdilla(70, true));*/
 
-        System.out.println(ejercicios.multa(60, false));
+        /*System.out.println(ejercicios.multa(60, false));
         System.out.println(ejercicios.multa(65, false));
-        System.out.println(ejercicios.multa(65, true));
+        System.out.println(ejercicios.multa(65, true));*/
 
+        
+       /* System.out.println(ejercicios.numeroVanidos(22));
+        System.out.println(ejercicios.numeroVanidos(23));
+        System.out.println(ejercicios.numeroVanidos(24));*/
+       
+        System.out.println(ejercicios.suenaMovil(false,false,false));
+        System.out.println(ejercicios.suenaMovil(false,false,true));
+        System.out.println(ejercicios.suenaMovil(true,false,false));
     }
 
 }

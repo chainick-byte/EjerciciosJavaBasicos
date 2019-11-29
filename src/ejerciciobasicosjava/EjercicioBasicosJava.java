fbb2fe7ab971a11c8356e838eebadf70f3036b8e
xp <xp@CETYSDAM00>
2019-11-29 19:52:05 +0100

    todos hechos mas ejercicio de palindromo planteado

--- src/ejerciciobasicosjava/EjercicioBasicosJava.java
+++ src/ejerciciobasicosjava/EjercicioBasicosJava.java
@@ -7,57 +7,49 @@ package ejerciciobasicosjava;
  * ejercicio 1A) el da leas ardillas!
  */
 public class EjercicioBasicosJava {
-    
-    
-    public boolean menorPro(int a,int b, int c){
-       if((a<10) ||(b<10) || (c<10)){
-           
-       }
-             
-            
-            
-        
-       return false; 
+
+    public boolean menorPro(int a, int b, int c) {
+        if ((a < 10) || (b < 10) || (c < 10)) {
+
+        }
+
+        return false;
     }
-    
-    
-    
-    public boolean suenaMovil(boolean matinal,boolean madre, boolean dormido){
-        if(matinal && madre && !dormido){
+
+    public boolean suenaMovil(boolean matinal, boolean madre, boolean dormido) {
+        if (matinal && madre && !dormido) {
             return true;
         }
-        if(!matinal && !madre && dormido ){
+        if (!matinal && !madre && dormido) {
             return false;
-            
+
         }
-        if ( dormido){
+        if (dormido) {
             return false;
-            
+
         }
-        if(matinal && !madre && !dormido){
+        if (matinal && !madre && !dormido) {
             return false;
         }
-        
+
         return true;
     }
-    
-    public boolean numeroVanidos(int numero){
-        
-       int numero_1=numero%11;
-       
-       if(numero_1==0){
-           return true;
-       }
-       if (numero_1==1){
-           return true;
-           
-       }
-            
+
+    public boolean numeroVanidos(int numero) {
+
+        int numero_1 = numero % 11;
+
+        if (numero_1 == 0) {
+            return true;
+        }
+        if (numero_1 == 1) {
+            return true;
+
+        }
+
         return false;
-        
-   
+
     }
-    
 
     public boolean fiestaArdilla(int numeroBellotas, boolean finSemana) {
         if (finSemana == true) {
@@ -91,6 +83,79 @@ public class EjercicioBasicosJava {
         return 0;
     }
 
+    public boolean tresEnteros(int a, int b, int c) {
+
+        if (Math.abs(c - a) >= 10 || Math.abs(a - b) >= 10 || Math.abs(b - c) >= 10) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public boolean dosEnteros(int a, int b) {
+        if (a / 10 == b / 10 || a % 10 == b % 10 || a / 10 == b % 10 || b / 10 == a % 10) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean multiplo(int a) {
+        int num1 = 3;
+        int num2 = 5;
+        if (a % num1 == 0 && a % num2 != 0) {
+            return true;
+        }
+          if (a % num1 != 0 && a % num2 == 0) {
+            return true;
+        }
+
+        if (a % 3 == 0 && a % 5 == 0) {
+            return false;
+        }
+
+        return false;
+
+    }
+    
+    public int billeteLoteria(int a, int b, int c){
+         
+        if((a==b)&&(b==c)&&(a==c)&&(a==2)){
+            return 10;
+        }
+         if((a==b)&&(b==c)&&(a==c)&&(a!=2)){
+            return 5;
+        }
+        return 0;
+    }
+    
+    public int sumaDados(int a,int b,boolean noDouble){
+        if((a==b) && noDouble==true){
+            return a+b+1;
+        }   
+        return a+b;
+    }
+    public boolean palindormo(String s){
+      String  auxiliar="";
+      int numero=s.length();
+        for(int i =0;i<numero;i++){
+            if(s.charAt(i)!=' '){
+                auxiliar=auxiliar+s.charAt(i);
+            }
+        }
+        for(int i=0; i<numero;i++){
+         String auxiliarInversa =auxiliar+s.charAt(numero-i);
+        }
+        if(auxiliar.equals(auxiliarInversa)){
+            return true;
+        }
+     return false;
+      
+  
+       
+      
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -100,18 +165,39 @@ public class EjercicioBasicosJava {
         System.out.println(ejercicios.fiestaArdilla(50, false));
         System.out.println(ejercicios.fiestaArdilla(70, true));*/
 
-        /*System.out.println(ejercicios.multa(60, false));
+ /*System.out.println(ejercicios.multa(60, false));
         System.out.println(ejercicios.multa(65, false));
         System.out.println(ejercicios.multa(65, true));*/
-
-        
-       /* System.out.println(ejercicios.numeroVanidos(22));
+ /* System.out.println(ejercicios.numeroVanidos(22));
         System.out.println(ejercicios.numeroVanidos(23));
         System.out.println(ejercicios.numeroVanidos(24));*/
-       
-        System.out.println(ejercicios.suenaMovil(false,false,false));
+ /*System.out.println(ejercicios.suenaMovil(false,false,false));
         System.out.println(ejercicios.suenaMovil(false,false,true));
-        System.out.println(ejercicios.suenaMovil(true,false,false));
+        System.out.println(ejercicios.suenaMovil(true,false,false));_*/
+ /*System.out.println(ejercicios.tresEnteros(1,7,11));
+        System.out.println(ejercicios.tresEnteros(1,7,10));
+        System.out.println(ejercicios.tresEnteros(11,1,7));*/
+ /*System.out.println(ejercicios.dosEnteros(12, 23));
+        System.out.println(ejercicios.dosEnteros(12, 43));
+        System.out.println(ejercicios.dosEnteros(12, 44));*/
+       /* System.out.println(ejercicios.multiplo(3));
+        System.out.println(ejercicios.multiplo(10));
+        System.out.println(ejercicios.multiplo(15));*/
+
+       /*System.out.println(ejercicios.billeteLoteria(2,2,2));
+         System.out.println(ejercicios.billeteLoteria(2,2,1));
+           System.out.println(ejercicios.billeteLoteria(0,0,0));*/
+       
+         /*System.out.println(ejercicios.sumaDados(2,3,true));
+         System.out.println(ejercicios.sumaDados(3,3,true));
+         System.out.println(ejercicios.sumaDados(3,3,false));
+         System.out.println(ejercicios.sumaDados(6,6,true));*/
+
+
+                  System.out.println(ejercicios.palindormo("taccat"));
+
+
+                
     }
 
 }
